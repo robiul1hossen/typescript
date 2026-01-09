533c149ae60c80df35ca42ce94b17620e141089b
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-09 20:08:51 +0600

    learn access modifiers in ts

---
+++ module3/3.5.ts
@@ -0,0 +1,37 @@
+{
+  //
+
+  // access modifiers
+
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number;
+
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
+
+    addDeposit(amount: number) {
+      return (this._balance = this._balance + amount);
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    test() {
+      this._balance;
+    }
+  }
+
+  const gorib = new BankAccount(12, "mr.gorib", 25);
+  const res = gorib.getBalance();
+  console.log(res);
+
+  //
+}
